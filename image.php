9f67a5a3c29d116db995c214a3785bb33d059db8
Ben Brooks <ben@bbrks.me>
2015-01-28 17:45:08 +0000

    Add getLatestItem function

--- image.php
+++ image.php
@@ -10,3 +10,19 @@ if (isset($_GET['url'])) {
   // We can't continue without a feed, so die()
   die('Please supply a URL parameter: ?url=http://example.com/feed.atom');
 }
+
+function getLatestItem($feed_url) {
+
+    $content = file_get_contents($feed_url);
+    $x = new SimpleXmlElement($content);
+
+    $item['title'] = $x->channel->item[0]->title;
+    $item['description'] = $x->channel->item[0]->description;
+    $item['link'] = $x->channel->item[0]->link;
+    $item['pubDate'] = $x->channel->item[0]->pubDate;
+
+    return $item;
+
+}
+
+$item = getLatestItem($feed_url);
